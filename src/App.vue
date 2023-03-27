a54f5ded728615becf6ab0f7755ba61aacce3adc
Jankku <29043938+Jankku@users.noreply.github.com>
2023-03-27 16:11:10 +0300

    feat: page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <router-view v-slot="{ Component, route }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="route.path" />
+    </transition>
+  </router-view>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
